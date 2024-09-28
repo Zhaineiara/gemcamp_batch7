9c047dcde6bfaa7cbdda2b975284684a95a16502
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-09-29 01:10:48 +0800

    Added home route with rails generate controller

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root 'welcome#index'
+  get '/home', to: 'new#home'
   get '/about_us', to: 'new#about'
   get '/contact', to: 'welcome#contact'
 end
