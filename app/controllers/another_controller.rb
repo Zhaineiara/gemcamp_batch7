d89ac8fd267c22a8c5bc61a79867158b4184892b
Precious Mapa <preciousdaniellamapa@gmail.com>
2024-10-01 11:03:42 +0800

    rails generate experiment

---
+++ app/controllers/another_controller.rb
@@ -0,0 +1,10 @@
+class AnotherController < ApplicationController
+  def another_file
+  end
+
+  def action1
+  end
+
+  def action2
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'another/another_file'
+  get 'another/action1'
+  get 'another/action2'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
@@ -6,4 +9,7 @@ Rails.application.routes.draw do
   get '/home', to: 'new#home'
   get '/about_us', to: 'new#about'
   get '/contact', to: 'welcome#contact'
+  get '/another', to: 'another#another_file'
+  get '/action1', to: 'another#action1'
+  get '/action2', to: 'another#action2'
 end
